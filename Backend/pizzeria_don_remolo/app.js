2f93e4566ef5285d895c11fb0f85b5d553007fba
Emanuel Arroyo <ema201217@gmail.com>
2022-02-02 01:37:31 -0300

    Add Connection to MongoDB Atlas

--- Backend/pizzeria_don_remolo/app.js
+++ Backend/pizzeria_don_remolo/app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config(); // Configuración de variables de entorno.
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const createError = require("http-errors");
+const path = require("path");
+const app = express();
+const mongoose = require('mongoose');
 
-var app = express();
+/////////////// Connection database /////////////
+mongoose.connect(process.env.URI_MONGODB_CONNECTION)
+.then(()=>console.log("Connection to MongoDB Atlas successful"))
+.catch(error=>console.error(error.message))
+/////////////////////////////////////////////////
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Enrutadores
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-app.use(logger('dev'));
+// Middlewares
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Rutas
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Error 404 -  Not Found
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
+// Error handler
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
 
 module.exports = app;
