95ced9688a5201c4f4fc28a183b1d9da0b7cfa12
Emanuel Arroyo <ema201217@gmail.com>
2022-02-09 16:12:32 -0300

    Arregle validacion de la segunda contraseña con un handleChangePass2

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,22 +1,21 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { registerAction } from '../redux/reducer';
-
-
+import { registerAction } from "../redux/reducer";
 
 export const Register = () => {
-  const password_confirm = useRef()
-
-  const dispatch = useDispatch()
-
+  const dispatch = useDispatch();
+  const [errorPass2, setErrorPass2] = useState();
+  const pass1 = useRef()
   const initialForm = {
     name: "",
     email: "",
     password: "",
-    rol: 1
+    rol: 1,
   };
-  const [form, setForm] = useState(initialForm)
-  const errors = useSelector(store=>store.data.errors)
+
+  const [form, setForm] = useState(initialForm);
+
+  const errors = useSelector((store) => store.data.errors);
 
   const handleChange = (e) => {
     setForm({
@@ -25,95 +24,69 @@ export const Register = () => {
     });
   };
 
+  const handleChangePass2 = (e) => {
+    setErrorPass2(
+      pass1.current?.value !== e.target.value
+        ? "Las contraseñas no coinciden"
+        : ''
+    )
+  }
   // Submit
   const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(registerAction(form))
-    
-  }
+    e.preventDefault();
+    dispatch(registerAction(form));
+  };
 
- /*   function validate(){
-      let input = form;
-      let errors = {};
-      let isValid = true;
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="form_login">
+        <label htmlFor="name">Nombre</label>
+        <input
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          placeholder="Enter name"
+          id="name"
+        />
+        <div className="text-danger">{errors && errors["name"]?.msg}</div>
 
-      if (!input.name) {
-        isValid = false;
-        errors.name = "Por favor ingrese su nombre";
-      }
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          placeholder="Enter email"
+          id="email"
+        />
 
-      if (!input.email) {
-        isValid = false;
-        errors.email = "Por favor ingrese su email.";
-      }
+        <div className="text-danger">{errors && errors["email"]?.msg}</div>
 
-      if (!input.password) {
-        isValid = false;
-        errors.password = "Por favor ingrese su contraseña.";
-      }
-  
-      if (!password_confirm) {
-        isValid = false;
-        errors.password_confirm = "Por favor confirme la contraseña.";
-      }
-  
-      if (typeof input["password"] !== "undefined" && typeof password_confirm !== "undefined") {
-          
-        if (input.password !== password_confirm) {
-          isValid = false;
-          errors.password_confirm = "Las contraseñas no coinciden.";
-        }
-      } 
+        <label htmlFor="password">Contraseña</label>
+        <input
+          type="password"
+          name="password"
+          ref={pass1}
+          value={form.password}
+          onChange={handleChange}
+          placeholder="Enter password"
+          id="password"
+        />
+        <div className="text-danger">{errors && errors["password"]?.msg}</div>
 
-      return isValid;
-  } */
+        <label htmlFor="password">Confirme la contraseña</label>
+        <input
+          type="password"
+          name="password_confirm"
+          placeholder="Enter confirm password"
+          id="confirm_password"
+          onChange={handleChangePass2}
+        />
+        <div className="text-danger">{errorPass2}</div>
 
-  return <div>
-        <form onSubmit={handleSubmit} className="form_login">
-            <label htmlFor="name">Nombre</label>
-            <input 
-              type="text" 
-              name="name" 
-              value={form.name}
-              onChange={handleChange}
-              placeholder="Enter name" 
-              id="name" />
-         <div className="text-danger">{errors['name']?.msg}</div>
-              
-            <label htmlFor="email">Email</label>
-            <input 
-              type="text" 
-              name="email" 
-              value={form.email}
-              onChange={handleChange}
-              placeholder="Enter email" 
-              id="email" />
-  
-             <div className="text-danger">{errors['email']?.msg}</div>
-   
-            <label htmlFor="password">Contraseña</label>
-            <input 
-              type="password" 
-              name="password" 
-              value={form.password}
-              onChange={handleChange}
-              placeholder="Enter password" 
-              id="password" />
-              <div className="text-danger">{errors['password']?.msg}</div>
-  
-  
-            <label htmlFor="password">Confirme la contraseña</label>
-            <input 
-              type="password" 
-              name="password_confirm"
-              ref={password_confirm}
-              placeholder="Enter confirm password" 
-              id="confirm_password" />
-             <div className="text-danger">{errors['password']?.value !== password_confirm.current?.value && "Las contraseñas no coinciden"}</div>
-  
-              
-          <button type="submit">Registrarse</button>
-        </form>
-      </div>;
+        <button type="submit">Registrarse</button>
+      </form>
+    </div>
+  );
 };
-
