6c7e105abbe8ce3add6b95082801ccb01da2bc30
Emanuel Constancio <emanuelconstancio.dev@gmail.com>
2022-03-30 12:10:34 -0300

    Details

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,46 +3,49 @@ import { useDispatch, useSelector } from "react-redux";
 import { loginAction } from '../redux/authReducer';
 import useForm from '../hooks/useForm'
 
+
 export const Login = () => {
-  const dispatch = useDispatch()
-  const [formValues, handleChange] = useForm({ email : '', password : '' })
-  const { email, password } = formValues;
-  const errors = useSelector(state => state.auth.errors)
+    const dispatch = useDispatch()
+    const [formValues, handleChange] = useForm({ email: '', password: '' })
+    const { email, password } = formValues;
+    const errors = useSelector(state => state.auth.errors)
 
-  // Submit
-  const handleSubmit = (e) => {
-    e.preventDefault()
+    // Submit
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
-    dispatch(loginAction(formValues))
-  }
+        dispatch(loginAction(formValues))
+    }
 
-  return <>
-      <form onSubmit={handleSubmit} className='form_login'>
-          
-          <label htmlFor="email">Email</label>
-          <input id='email' 
-          name="email"
-          type="email"
-          placeholder='Email'
-          value={email}
-          onChange={handleChange}
-          ></input>
+    return <>
+        <form onSubmit={handleSubmit} className='form_login'>
 
-          <div className="text-danger">{errors && errors["email"]?.msg}</div>
+            <label htmlFor="email">Email</label>
+            <input
+                id='email'
+                name="email"
+                type="email"
+                placeholder='Email'
+                value={email}
+                onChange={handleChange}
+            ></input>
 
-          <label htmlFor="password">Contraseña</label>
-          <input id='password' 
-          type="password"
-          name="password"
-          placeholder='Contraseña'
-          value={password}
-          onChange={handleChange}
-          ></input>
+            <div className="text-danger">{errors && errors["email"]?.msg}</div>
 
-          <div className="text-danger">{errors && errors["password"]?.msg}</div>
+            <label htmlFor="password">Contraseña</label>
+            <input 
+                id='password'
+                type="password"
+                name="password"
+                placeholder='Contraseña'
+                value={password}
+                onChange={handleChange}
+            ></input>
+
+            <div className="text-danger">{errors && errors["password"]?.msg}</div>
 
 
-          <button type='submit'>Ingresar</button>
-      </form>
-  </>;
+            <button type='submit'>Ingresar</button>
+        </form>
+    </>;
 };
