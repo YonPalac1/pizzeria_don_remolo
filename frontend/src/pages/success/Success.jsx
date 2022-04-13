5bc6fb68d970a848cc72f83a2fec1260acbaec58
Emanuel Constancio <emanuelconstancio.dev@gmail.com>
2022-04-13 12:58:01 -0300

    404

--- frontend/src/pages/success/Success.jsx
+++ frontend/src/pages/success/Success.jsx
@@ -10,40 +10,39 @@ import './success.css'
 export const Success = () => {
 
     const orderStatus = useSelector(state => state.cart.orderStatus) // here is the flag of the order
-  
+
     const order = useSelector(state => state.cart.order)
-    const orderId = 20;  //we need this data from the endpoind
+    const orderId = 20;  //we need this data from the Endpoind
     const dispatch = useDispatch();
     const flag = true    //Also this...
 
     useEffect(() => {
-      dispatch(makeOrder(order))
-      // console.log(orderStatus)
-      // console.log(order)
-    },[Success])
-    
-         
+        dispatch(makeOrder(order))
+        // console.log(orderStatus)
+        // console.log(order)
+    }, [Success])
+
+
     if (orderStatus.ok !== true) {
-        return <Error/>
+        return <Error />
     }
 
 
     // console.log(orderStatus)
-  return (
-    <div className='main'>
-      <SuccessIcon/>
-      <span className='span1'>Pedido confirmado</span>
-      <span className='span2'>Orden #{orderId}</span>
-      <span className='span3'>
-          
-        {
-          flag === true ? message.toTakeAway:
-          message.toHome 
-            
-        }
+    return (
+        <div className='main'>
+            <SuccessIcon />
+            <span className='span1'>Pedido confirmado</span>
+            <span className='span2'>Orden #{orderId}</span>
+            <span className='span3'>
 
-      </span>
-    </div>
-  )
+                {
+                    flag === true ? message.toTakeAway :
+                        message.toHome
+
+                }
+
+            </span>
+        </div>
+    )
 }
- 
