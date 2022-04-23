c649cf271cc3911ef34c144aab0df5bc98879fa9
Emanuel <emanuelcosntancio.dev@gmail.com>
2022-04-23 14:58:28 -0300

    DeliveryTable Created

---
+++ frontend/src/components/Backoffice/DeliveryTable/DeliveryTable.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const DeliveryTable = props => {
+  return (
+    <div>DeliveryTable</div>
+  )
+}
+
+DeliveryTable.propTypes = {
+    
+}
+
+// react arrow funtional component propTypes  RAFCP
+
